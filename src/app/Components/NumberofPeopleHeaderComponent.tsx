7e16f3807b28035d5c757efc7093de452eccc3f4
itzteeler <tylernguyen518@gmail.com>
2024-04-04 13:59:18 -0700

    rules

--- src/app/Components/NumberofPeopleHeaderComponent.tsx
+++ src/app/Components/NumberofPeopleHeaderComponent.tsx
@@ -4,7 +4,7 @@ const NumberofPeopleHeader = (props:{title: string; error: boolean}) => {
   return (
     <div className='flex flex-col md:flex-row justify-between'>
         <p className='text-[color:var(--textColor)] text-[16px] font-[Space-Mono-Bold]'>{props.title}</p>
-        {props.error && <p className='font-[Space-Mono-Bold] text-[color:var(--error)]'>Can\t be zero</p>}
+        {props.error && <p className='font-[Space-Mono-Bold] text-[color:var(--error)]'>Can't be zero</p>}
     </div>
   )
 }
